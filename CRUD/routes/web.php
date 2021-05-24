556bfec2677747f0cd5b0d8f5a06121c1d5da5d6
SC <chaolic6505@gmail.com>
2021-05-24 03:27:17 -0700

    Configure tailwind

--- CRUD/routes/web.php
+++ CRUD/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CarController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/users',function(){
-    return 'welcome';
-});
+Route::resource('/', CarController::class);
